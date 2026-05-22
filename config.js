007527836c467a2e29c05ff65b25c70ce4802802
Camalr <camalrene@unisabana.edu.co>
2026-05-22 08:59:15 -0500

    config.js

---
+++ config.js
@@ -0,0 +1,22 @@
+// papel-y-luna/config/config.js
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: false,
+    },
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  },
+}; 
