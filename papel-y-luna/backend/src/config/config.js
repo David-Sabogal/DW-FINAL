258b5a90e011aa01e7b8d57cf79dac0bfdfa70d2
Camalr <camalrene@unisabana.edu.co>
2026-05-22 09:10:13 -0500

    FINAL

--- papel-y-luna/backend/src/config/config.js
+++ papel-y-luna/backend/src/config/config.js
@@ -1,18 +1,15 @@
 require('dotenv').config();
-
 module.exports = {
   development: {
-    dialect: 'sqlite',
-    storage: './database.sqlite',
-    logging: false,
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    dialectOptions: { ssl: false },
   },
   production: {
-    dialect: 'mysql',
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 3306,
-    database: process.env.DB_NAME,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    logging: false,
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: { require: true, rejectUnauthorized: false },
+    },
   },
 };
